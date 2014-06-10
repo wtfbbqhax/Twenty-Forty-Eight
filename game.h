91c976af46b5fd276d26c03d265bf0488a6bbb50
wtfbbqhax <wtfbbqhax@users.noreply.github.com>
2014-06-10 14:40:37 -0400

    whitespace fix

--- game.h
+++ game.h
@@ -1,25 +1,73 @@
-/* GameBoard.h */
-#pragma once
+#include <stdint.h>
 
-#define COLS (4)
-#define ROWS (4)
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+enum Move { Up, Down, Left, Right };
+
+typedef uint16_t            Tile;
+typedef array<Tile,4>       GridLine;
+typedef array<GridLine,4>   _Grid;
+
+struct Grid : _Grid {
+    typedef pair<int,int> grid_iterator;
+
+    Grid() { clear(); }
+
+    void clear()
+    {
+        GridLine _nil = {{0,0,0,0}};
+        fill(_nil);
+    }
+};
 
 class Game {
-  private:
-	unsigned board[COLS][ROWS];
-	bool Update(void);
-	bool Peek(void);
+    typedef vector< class Observer * > observer_type;
+    observer_type views;
+    Grid _grid;
+    unsigned _score;
+    bool gameover;
+public:
+    Game() { reset(); }
 
-  public:
-    enum Direction {
-        UP, DOWN, LEFT, RIGHT 
-    };
+    void reset() {
+        gameover = false;
+        _grid.clear();
+        _score = 0;
+        addRandomTile();
+        addRandomTile();
+        notify();
+    } 
 
-	//Game();
-    //virtual ~Game();
+    Grid grid() { return _grid; } 
+    unsigned score() { return _score; }
+    bool gameOver() { return gameover; }
+    void move(Move m); // { notify(); };
+    bool addRandomTile();
+    bool addRandomTile(Tile val);
+    void notify();
+    void attach(Observer *obs) { views.push_back(obs); }
+};
 
-    void Reset(void);
-	bool Move( Direction );
-	void Print(void);
-	bool GameOver(void);
+class Observer {
+    Game *_model;
+protected:
+    Game* model() { return _model; }
+public:
+    Observer(Game *mod) {
+        _model=mod;
+        _model->attach(this);
+    }
+    virtual ~Observer() { }
+    virtual void update() = 0;
+};
+
+class Display: public Observer {
+public:
+    Display(Game *g): Observer(g) { }
+    void update();
 };
